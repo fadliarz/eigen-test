fa8e507ba1996cc49ea5ccb3b4c16a9751df2e84
fadliarz <fadliar03@gmail.com>
2024-09-23 19:37:51 +0700

    update

--- dist/src/modules/loan/loan.type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoanDITypes = void 0;
-exports.LoanDITypes = {
-    REPOSITORY: Symbol.for("LOAN_REPOSITORY"),
-    SERVICE: Symbol.for("LOAN_SERVICE"),
-    CONTROLLER: Symbol.for("LOAN_CONTROLLER"),
-};
--- dist/src/modules/member/member.type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MemberDITypes = void 0;
-exports.MemberDITypes = {
-    REPOSITORY: Symbol.for("MEMBER_REPOSITORY"),
-    SERVICE: Symbol.for("MEMBER_SERVICE"),
-    CONTROLLER: Symbol.for("MEMBER_CONTROLLER"),
-};
